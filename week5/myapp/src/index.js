804cc0beb8c5d38001ae872ac483eb6d979d6429
sauravacharya-stake <sauravacharya169@gmail.com>
2025-10-30 17:24:34 +0000

    Week 5

---
+++ week5/myapp/src/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import AppColor from './AppbackgroundColor';
+import reportWebVitals from './reportWebVitals';
+import GreetingElement from './myGreetingApp'
+import GreetingElementwithProp from './mygreetingprop';
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+  <AppColor heading="This is first element" lbl="Name :" 
+color="green"/>
+<AppColor heading="This is second element" lbl="Name :" 
+color="blue"/> 
+ <AppColor heading="This is third third element" lbl="Name :" 
+color="Yellow"/>
+<GreetingElementwithProp msg="Hi its Monday"/>
+<GreetingElementwithProp msg="Hi its tuesday"/>
+<GreetingElementwithProp msg="Hi its wednesday"/>
+<GreetingElementwithProp msg="Hi its thursday"/>
+  </React.StrictMode >
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
