bb57649dbe36b7bf86438b454c7a2b127450d595
sauravacharya-stake <sauravacharya169@gmail.com>
2025-11-06 16:34:02 +0000

    week 1

--- week1/index.js
+++ week1/index.js
@@ -1,2 +1,24 @@
 console.log("saurav acharya")
-console.log("jatha ke aayena")
+console.log("hi its me saurav")
+
+
+// app.js or index.js
+const prompt = require('prompt-sync')(); // This line is essential
+console.log("starting")
+const name = prompt('Enter your name: ');
+console.log(`Hello, ${name}`);
+// program that checks if the number is positive, negative or zero
+// input from the user
+const number = parseInt(prompt("Enter a number: "));
+// check if number is greater than 0
+if (number > 0) {
+console.log("The number is positive");
+}
+// check if number is 0
+else if (number == 0) {
+console.log("The number is zero");
+}
+// if number is less than 0
+else {
+console.log("The number is negative");
+}
